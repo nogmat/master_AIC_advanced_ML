4f389a0eb25ff291883deb1e09a199ff3b058e3f
Baptiste Merliot <baptmer@laposte.net>
2020-01-31 14:20:28 +0100

    corrected netVLAD

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,13 +1,17 @@
 from keras.applications import VGG16
 from keras.models import Sequential
 
-class Feature_Extractor:
 
+class Feature_Extractor:
     def __init__(self, image_size):
 
         self.model = Sequential()
 
-        vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
+        vgg16 = VGG16(
+            weights="imagenet",
+            include_top=False,
+            input_shape=(image_size, image_size, 3),
+        )
         # vgg16.layers[-3] is block5_conv2
         # vgg16.layers[-2] is block5_conv3
         # vgg16.layers[-1] is block5_pool
--- netVLAD.py
+++ netVLAD.py
@@ -1,60 +1,61 @@
 import numpy as np
 
-class netVLAD:
 
+class netVLAD:
     def __init__(self, kmeans, alpha=5000.0, n_cluster=32):
         self.alpha = alpha
         self.n_clusters = n_cluster
         self.kmeans = kmeans
 
-    def similarity(self, F):
+    def Fc(self, F):
+        """Computing difference Fc_ijkd[k]
+        input shape: F[i,j,d] 
+        output shape: Fc[i,j,k,d]
+        """
+
+        F_ijkd = np.repeat(F[:, :, np.newaxis, :], self.n_clusters, axis=2)
+
+        c_ijkd = np.repeat(
+            np.repeat(
+                self.kmeans.cluster_centers_[np.newaxis, :, :],
+                F.shape[1],
+                axis=0,
+            ),
+            F.shape[0],
+            axis=0,
+        )
+
+        return F_ijkd - c_ijkd
+
+    def similarity(self, Fc_ijkd):
         """Computing similarity S_ij[k]
         input shape: F[i,j,d] 
         output shape: S[k,i,j]
         """
-        norm_Fc_ijk = np.linalg.norm(
-            np.repeat(
-                F[:, :, np.newaxis, :], self.n_clusters, axis=2
-            ) - np.repeat(
-                    np.repeat(
-                        self.kmeans.cluster_centers_[np.newaxis, :, :],
-                        F.shape[1], axis=0
-                    ),
-                    F.shape[0], axis=0
-                ),
-            axis = 3
-        )
-        exp_Fc_ijk = np.exp(- self.alpha * norm_Fc_ijk)
+
+        norm_Fc_ijk = np.linalg.norm(Fc_ijkd, axis=3)
+        exp_Fc_ijk = np.exp(-self.alpha * norm_Fc_ijk)
+
         S_ijk = np.divide(
             exp_Fc_ijk,
             np.repeat(
                 np.sum(exp_Fc_ijk, axis=2)[:, :, np.newaxis],
-                self.n_clusters, axis=2
-            )
+                self.n_clusters,
+                axis=2,
+            ),
         )
-        return np.moveaxis(
-            S_ijk,
-            [0,1,2],
-            [2,0,1]
-        )
+
+        return S_ijk
 
     def V(self, F):
         """Computing V[d,k]
         input shape: F[i,j,d]
         output shape: V[d,k]
         """
-        S_dkij = np.moveaxis(
-            np.repeat(self.similarity(F)[:, :, :, np.newaxis], F.shape[2], axis=3),
-            [0,1,2,3],
-            [3,0,1,2]
-        )
-        Fc_dkij = np.repeat(
-            F[:, :, :, np.newaxis], self.n_clusters, axis=3
-            ) - self.kmeans.cluster_centers_
-        return np.sum(
-            np.sum(
-                np.multiply(S_dkij, Fc_dkij),
-                axis=3
-            ),
-            axis=2
-        )
+
+        Fc_ijkd = self.Fc(F)
+        S_ijk = self.similarity(Fc_ijkd)
+
+        S_ijkd = np.repeat(S_ijk[:, :, :, np.newaxis], F.shape[2], axis=3)
+        return np.sum(np.sum(np.multiply(S_ijkd, Fc_ijkd), axis=1), axis=0)
+
