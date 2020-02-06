60b50a24de879a062f6fa4ddeb3030d97c529cb5
Matthieu Nogatchewsky <matnog@hotmail.fr>
2020-02-06 16:46:59 +0100

    Unstable version - netVLAD

--- netVLAD_v2.py
+++ netVLAD_v2.py
@@ -6,80 +6,128 @@ from __future__ import absolute_import, division, print_function, \
 import tensorflow as tf
 
 
+class Addition(tf.keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super(Addition, self).__init__(**kwargs)
+
+    def call(self, x):
+        assert(isinstance(x, list))
+        assert(len(x) >= 2)
+        return tf.math.add_n(x)
+
+
 class NetVLAD(tf.keras.layers.Layer):
 
-    def __init__(self, alpha, kmeans, **kwargs):
+    def __init__(self, alpha, **kwargs):
         self.alpha = alpha
-        self.kmeans = kmeans
+        self.trainable = True
         super(NetVLAD, self).__init__(**kwargs)
 
-    def diff(self, a, c):
-        v = tf.keras.backend.sum(tf.math.multiply(
-            a-c, a-c), axis=2, keepdims=True)
-        v = tf.math.exp(-self.alpha * v)
-        return v
+    def call(self, inputs):
 
-    def similarities(self, inputs):
-        S_ij_k = []
-        assert(inputs.shape[:-1] == self.kmeans.cluster_centers_[:-1])
-        for c_k in self.kmeans.cluster_centers_:
-            S_ij_k.append(self.diff(inputs, c_k))
-        S_ij_sum = tf.math.add_n(S_ij_k)
-        for k in range(len(S_ij_k)):
-            S_ij_k[k] = tf.math.divide(S_ij_k[k], S_ij_sum)
-        return tf.keras.backend.concatenate(S_ij_k, axis=2)
+        # TEST ENVIRONMENT ##
+        # kmeans_centers = tf.constant([[1.0, 1.0], [2.0, 3.0]])
+        # features = inputs
+        # print(features.shape)
+        # features = tf.keras.backend.repeat_elements(
+        #     features,
+        #     rep=2,
+        #     axis=2)
+        # features = tf.keras.backend.reshape(
+        #     features,
+        #     shape=(inputs.shape[0], 2, 2, 2, 2))
+        # return [features]
 
-    def residuals(self, inputs):
-        R_ij_k = []
-        for c_k in self.kmeans.cluster_centers_:
-            R_ij_k.append(inputs - tf.Variable(c_k))
-        R_ij_k = tf.keras.backend.concatenate(R_ij_k, axis=2)
+        return [inputs]
+        ######################
+
+        # assert(isinstance(inputs, list))
+        # assert(len(inputs) == 2)
+        # features = tf.cast(inputs[0], dtype=tf.float32)
+        # kmeans_centers = tf.cast(inputs[1], dtype=tf.float32)
+        # assert(len(list(features.shape)) == 3)
+        # assert(len(list(kmeans_centers.shape)) == 2)
+        # assert(features.shape[-1] == kmeans_centers.shape[-1])
+
+        # F = tf.keras.backend.reshape(
+        #     tf.keras.backend.repeat_elements(
+        #         features,
+        #         rep=kmeans_centers.shape[0],
+        #         axis=1),
+        #     shape=(
+        #         features.shape[0],
+        #         features.shape[1],
+        #         kmeans_centers.shape[0],
+        #         features.shape[2]))
+        # c = kmeans_centers
+        # S = tf.math.exp(
+        #     -self.alpha *
+        #     tf.keras.backend.sum(
+        #         tf.keras.backend.pow(
+        #             tf.math.subtract(F, c),
+        #             2),
+        #         axis=3))
+        # sum_S = tf.keras.backend.sum(
+        #     S,
+        #     axis=2)
+        # S = tf.transpose(
+        #     tf.math.divide(
+        #         tf.transpose(S),
+        #         tf.transpose(sum_S)))
+        # V = tf.keras.backend.sum(
+        #     tf.math.multiply(
+        #         tf.transpose(tf.math.subtract(F, c)),
+        #         tf.transpose(S)
+        #     ),
+        #     axis=2)
+        # V_h = tf.math.l2_normalize(
+        #     tf.keras.backend.reshape(
+        #         tf.transpose(V),
+        #         shape=(
+        #             kmeans_centers.shape[0]*kmeans_centers.shape[1],
+        #             1)),
+        #     axis=0,
+        #     epsilon=1e-12,
+        #     name=None
+        # )
+        # return [V_h, S]
+
+
+class SingleLayer(tf.keras.Model):
+
+    def __init__(self, layer_):
+        super(SingleLayer, self).__init__()
+        self.layer = layer_
 
     def call(self, inputs):
-        # inputs represents F
-        S = self.similarities(inputs)
-        R = self.residuals(inputs)
-        St, Rt = tf.transpose(S), tf.transpose(R)
-        ARtemp = tf.math.multiply(St, Rt)
-        AR = tf.keras.backend.sum(ARtemp, axis=1)
-        return [tf.keras.backend.l2_normalize(
-            AR.reshape(AR.shape[0]*AR.shape[1])
-        )]
+        return self.layer(inputs)
 
 
 if __name__ == "__main__":
 
-    # Tests
-    a = tf.Variable([[[4.0, 2.0], [2.0, 1.0]],
-                     [[1.0, 0.0], [3.0, 2.0]]
-                     ])
-    c1 = tf.Variable([1.0, 0.0])
-    c2 = tf.Variable([0.0, 1.0])
+    import numpy as np
+    features = tf.constant([[[1.0, 2.0], [3.0, 4.0]],
+                            [[5.0, 6.0], [7.0, 8.0]]])
+    # sums = tf.keras.backend.repeat_elements(
+    #     features,
+    #     rep=2,
+    #     axis=1)
+    # print(sums)
+    # sums = tf.keras.backend.reshape(
+    #     sums,
+    #     shape=(2, 2, 2, 2)
+    # )
+    # print(sums)
+    # kmeans_centers = tf.constant([[1.0, 1.0], [2.0, 3.0]])
+    # sums = tf.math.subtract(
+    #     sums,
+    #     kmeans_centers
+    # )
+    # print(sums)
+    # model = SingleLayer(netVLAD)
 
-    def diff(a, c):
-        v = a-c
-        v = tf.math.multiply(a-c, a-c)
-        v = tf.keras.backend.sum(v, axis=2, keepdims=True)
-        # v = tf.math.exp(-v)
-        return v
-
-    S_ij1 = diff(a, c1)
-    print(S_ij1)
-    S_ij2 = diff(a, c2)
-    print(S_ij2)
-    s_ijsum = tf.math.add_n([S_ij1, S_ij2])
-    print(s_ijsum)
-    S_ijk = tf.keras.backend.concatenate([S_ij1, S_ij2], axis=2)
-    print(S_ijk)
-    S_ijk = tf.transpose(S_ijk)
-    s_ijsum = tf.transpose(s_ijsum)
-    S_ijk /= s_ijsum
-    S_ijk = tf.transpose(S_ijk)
-    print(S_ijk)
-    # print(model.predict([[2, 3]]))
-
-    # Define VGG-16
-    # Pre-train using ImageNet dataset
-    # Define NetVLAD and adapt to the equations of descriptors
-    #   Don't forget loss functions
-    # Define discriminators
+    model = tf.keras.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(2, 2, 2))
+    ])
+    print(model.predict([features]))
