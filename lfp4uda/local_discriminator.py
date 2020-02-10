d4f63b9ab547645101d75ee9608323875901bc3a
Matthieu Nogatchewsky <matnog@hotmail.fr>
2020-02-10 14:45:20 +0100

    Activation like in discriminators.py

--- lfp4uda/local_discriminator.py
+++ lfp4uda/local_discriminator.py
@@ -1,5 +1,3 @@
-#!/bin/python
-
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
@@ -11,9 +9,9 @@ class LocalDiscriminator(tf.keras.layers.Layer):
 
     def __init__(self, nunits_1, nunits_2, **kwargs):
         super(LocalDiscriminator, self).__init__(**kwargs)
-        self.dense_1 = tf.keras.layers.Dense(nunits_1)
-        self.dense_2 = tf.keras.layers.Dense(nunits_2)
-        self.dense_3 = tf.keras.layers.Dense(1)
+        self.dense_1 = tf.keras.layers.Dense(nunits_1, activation='relu')
+        self.dense_2 = tf.keras.layers.Dense(nunits_2, activation='relu')
+        self.dense_3 = tf.keras.layers.Dense(1, activation='sigmoid')
 
     def call(self, inputs):
         output = self.dense_1(inputs)
